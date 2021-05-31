e146b6ab215589b77747a7f83e3aad74379afcc6
arush123 <biswal805@gmail.com>
2021-05-31 19:22:17 +0530

    Add files via upload

---
+++ alie-recom.js
@@ -0,0 +1,52 @@
+(function (window) {
+    // You can enable the strict mode uncommenting the following line  
+    'use strict';
+
+    // This function will contain all our code
+    function alieLibrary() {
+        var _alieLibraryObject = {};
+
+        // This variable will be inaccessible to the user, only can be visible in the scope of your library.
+        var settings = {
+            volume: 100,
+            mute: false
+        };
+
+
+        // Change a private property
+        _alieLibraryObject.setVolume = function (volume) {
+            settings.volume = volume;
+            return volume;
+        };
+
+        // Change a private property
+        _alieLibraryObject.setMute = function (muteStatus) {
+            if (typeof (muteStatus) === 'boolean') {
+                settings.mute = muteStatus;
+            } else {
+                console.error("You need to disable or enable the sound !");
+            }
+
+            return settings.mute;
+        };
+
+        // Change a private property
+        _alieLibraryObject.haveSound = function () {
+            return settings.mute;
+        };
+
+        return _alieLibraryObject;
+    }
+
+    // We need that our library is globally accesible, then we save in the window
+    if (typeof (window.alie) === 'undefined') {
+        window.alie = alieLibrary();
+    }
+})(window); // We send the window variable withing our function
+
+
+// Now see the content of your library
+console.log(alie);
+
+// It should ouput only 3 properties (setMute, setVolume, haveSound)
+// And the settings variable can be only be accessed in your library, not outside
